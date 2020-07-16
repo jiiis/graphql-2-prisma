8a93a733129b1949dd098faec0a9849906bb7edd
jiiis <yngwie.chang@gmail.com>
2020-07-17 00:35:11 +1000

    Extracted prisma secret to env var.

--- src/prisma.js
+++ src/prisma.js
@@ -5,7 +5,7 @@ import { fragmentReplacements } from './resolvers/index'
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret: 'erniuniu',
+  secret: process.env.PRISMA_SECRET,
   fragmentReplacements
 })
 
